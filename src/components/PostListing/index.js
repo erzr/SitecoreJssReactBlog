c548cbe4a6d04b67d5da4955a8ed4387fe580129
erzr <aelamarre@gmail.com>
2019-02-25 19:48:21 -0500

    stubed recent posts component.

---
+++ src/components/PostListing/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PostListingItem from '../PostListingItem';
+
+const PostListing = (props) => (
+  <div>
+      <PostListingItem />
+  </div>
+);
+
+export default PostListing;
---
+++ src/components/RecentPosts/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from '@sitecore-jss/sitecore-jss-react';
+import PostListing from '../PostListing';
+
+const RecentPosts = (props) => (
+  <div>
+    <PostListing />
+  </div>
+);
+
+export default RecentPosts;
